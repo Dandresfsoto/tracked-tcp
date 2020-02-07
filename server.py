965575b99c28c4614e7daeb7d701bc1b652328b0
dandresfsoto@gmail.com <maquinas2008%>
2020-02-07 16:39:33 -0500

    Initial commit

---
+++ server.py
@@ -0,0 +1,27 @@
+from tornado.ioloop import IOLoop
+from tornado.iostream import StreamClosedError
+from tornado.tcpserver import TCPServer
+
+
+class Server(TCPServer):
+    message_separator = b'\r\n'
+
+    async def handle_stream(self, stream, address):
+        while True:
+            try:
+                request = await stream.read_until(self.message_separator)
+            except StreamClosedError:
+                stream.close(exc_info=True)
+                return
+            try:
+                await stream.write(request)
+            except StreamClosedError:
+                stream.close(exc_info=True)
+                return
+
+
+if __name__ == '__main__':
+    Server().listen(5567)
+    print('Starting the server...')
+    IOLoop.instance().start()
+    print('Server has shut down.')
